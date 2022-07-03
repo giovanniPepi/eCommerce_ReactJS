0f7abae806d38c400650f131c5423e9c3b42c4e2
giovanniPepi <gtpepi@hotmail.com>
2022-07-03 11:41:47 -0300

    Add route switching

---
+++ src/components/BrowserRouter.js
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "../App";
+import Catalog from "./Catalog";
+
+const RouteSwitch = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/catalog" element={<Catalog />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default RouteSwitch;
