68543ea000f82e73613ad65e902429bf115c05e9
giovanniPepi <gtpepi@hotmail.com>
2022-07-06 13:47:48 -0300

    Add entries to processors databse

--- src/data/processors.js
+++ src/data/processors.js
@@ -6,6 +6,16 @@ import i912900KS from "../img/processors/Intel-Core-i9-12900KS.jpg";
 import i912900KSback from "../img/processors/Intel-Core-i9-12900KSback.jpg";
 import i12900kCoreI9 from "../img/processors/Intel-12900k-Core-I9.jpg";
 import i12900kCoreI9back from "../img/processors/Intel-12900k-Core-I9back.jpg";
+import ryzen75700g from "../img/processors/Amd-Ryzen-7-5700g.jpg";
+import ryzen75700gback from "../img/processors/Amd-Ryzen-7-5700gback.jpg";
+import i912900K from "../img/processors/i9-12900K.jpg";
+import i912900Kback from "../img/processors/i9-12900Kback.jpg";
+import i710700K from "../img/processors/i710700K.jpg";
+import i710700Kback from "../img/processors/i710700Kback.jpg";
+import I912900kf from "../img/processors/I912900kf.jpg";
+import I912900kfback from "../img/processors/I912900kfback.jpg";
+import Ryzen95900X from "../img/processors/Ryzen95900X.jpg";
+import Ryzen95900Xback from "../img/processors/Ryzen95900Xback.jpg";
 
 const processors = [
   {
@@ -23,56 +33,56 @@ const processors = [
     name: "Intel Core I9-10900X",
     price: 1399,
     description: `- Marca: Intel
-    - Modelo: Bx8069510900x
+  - Modelo: Bx8069510900x
 
-    ESPECIFICAÇÕES:
-    - Socket Processador: LGA2066
-    - Processador: Intel Core I9
-    - Garantia Fabricante: 1 Ano
-    - Velocidade Clock Processador: 3,7 GHz
-    - Tipos de Memória: DDR4, 2933 MHz
-    - Tecnologia Hyper-Threading da Intel®: Sim
-    - Cache processador: 19MB
-    - Arquitetura: 14 nm
-    - Número de Núcleos: 10
-    - Voltagem / TDP: 165W
-    - Vídeo Integrado (Gráficos do Processador): Não
+  ESPECIFICAÇÕES:
+  - Socket Processador: LGA2066
+  - Processador: Intel Core I9
+  - Garantia Fabricante: 1 Ano
+  - Velocidade Clock Processador: 3,7 GHz
+  - Tipos de Memória: DDR4, 2933 MHz
+  - Tecnologia Hyper-Threading da Intel®: Sim
+  - Cache processador: 19MB
+  - Arquitetura: 14 nm
+  - Número de Núcleos: 10
+  - Voltagem / TDP: 165W
+  - Vídeo Integrado (Gráficos do Processador): Não
 
-    * Não acompanha Cooler
+  * Não acompanha Cooler
 
-    Dimensões:
-    - Altura (bruto) [m]: 0.12
-    - Largura (bruto) [m]: 0.1
-    - Profundidade (bruto) [m]: 0.07
-    - Peso (bruto) [Kg]: 0.29
+  Dimensões:
+  - Altura (bruto) [m]: 0.12
+  - Largura (bruto) [m]: 0.1
+  - Profundidade (bruto) [m]: 0.07
+  - Peso (bruto) [Kg]: 0.29
 
-    - EAN: 5032037171717
+  - EAN: 5032037171717
 
-    CONTEÚDO DA EMBALAGEM:
-    - 1 Processador Intel Core I9-10900X Cascade Lake, 3.70 GHz
-
-
-
-    Garantia do Fornecedor
-
-    1 Ano
+  CONTEÚDO DA EMBALAGEM:
+  - 1 Processador Intel Core I9-10900X Cascade Lake, 3.70 GHz
 
 
-    Itens Inclusos
 
-    Processador Intel Core I9-10900x Cascade Lake 3,70 GHZ 19mb LGA 2066 - Bx8069510900x - sem Cooler - sem Video ON Board
+  Garantia do Fornecedor
 
-    Fabricante
-
-    Intel
+  1 Ano
 
 
-    Modelo
+  Itens Inclusos
 
-    Bx8069510900x
+  Processador Intel Core I9-10900x Cascade Lake 3,70 GHZ 19mb LGA 2066 - Bx8069510900x - sem Cooler - sem Video ON Board
 
-    Peso:
-    600 gramas(bruto com embalagem)`,
+  Fabricante
+
+  Intel
+
+
+  Modelo
+
+  Bx8069510900x
+
+  Peso:
+  600 gramas(bruto com embalagem)`,
     img: i910900x,
     imgback: i910900xback,
   },
@@ -87,7 +97,7 @@ const processors = [
 
 - Modelo: BX8071512900KS
 
- 
+
 
 Especificação Técnica:
 
@@ -145,7 +155,7 @@ Especificação Técnica:
 
 - ISM5: Sim
 
- 
+
 
 Conteúdo da Embalagem:
 
@@ -216,7 +226,7 @@ CONTEÚDO DA EMBALAGEM:
 
 - Processador Intel 12900k Core I9 1700 3.20 Ghz - Bx8071512900k - Ger 12
 
- 
+
 
 Garantia do Fornecedor
 
@@ -227,18 +237,376 @@ Peso:
     img: i12900kCoreI9,
     imgback: i12900kCoreI9back,
   },
+  {
+    category: "Processors",
+    id: "Amd-Ryzen-7-5700g",
+    name: "Processador Amd Ryzen 7 5700g, 3.8ghz (4.6ghz Max Turbo), Am4, Vídeo Integrado, 8 Núcleos",
+    price: 839,
+    description: `Marca: AMD- Modelo: 100-100000263BOXEspecificações: - Nº de núcleos de CPU: 8- Nº de threads: 16- Nº de núcleos de GPU: 8- Clock básico: 3.8GHz- Clock de Max Boost: Até 4.6GHz- Total de Cache L2: 4MB- Cachê L3 total: 16MB- Desbloqueado: Sim- CMOS: TSMC 7nm FinFET- Pacote: AM4- Versão PCI Express: PCIe® 3.0- Solução térmica: Wraith Stealth- TDP / TDP Padrão: 65W- cTDP: 45-65W- Temps máx: 95°CSuporte de SO: - Edição Windows 10 - 64-Bit- RHEL x86 64-Bit- Ubuntu x86 64-BitMemória: - Velocidade máxima da memória: Até 3200MHz- Tipo de memória: DDR4- Canais de memória: 2Especificações da placa de vídeo: - Frequência gráfica: 2000 MHz- Modelo gráfico: Radeon Graphics- Contagem de núcleos gráficos: 8Principais recursos: - Display Port- HDMIConteúdo da Embalagem- 1x Processador AMD Ryzen 7 5700G
+
+
+
+Garantia do Fornecedor
+
+12 meses
+
+Peso:
+434 gramas (bruto com embalagem)`,
+    img: ryzen75700g,
+    imgback: ryzen75700gback,
+  },
+  {
+    category: "Processors",
+    id: "Intel-Core-i9-12900K",
+    name: "Processador Intel Core i9-12900K, Cache 30MB, 3.2GHz (5.2GHz Max Turbo), LGA 1700 - BX8071512900K",
+    price: 769,
+    description: `ATENÇÃO:
+
+Características:
+
+- Marca: Intel
+
+- Modelo: BX8071512900K
+
+
+Especificações:
+
+- Intel Core i9-12900K
+
+- 12° Geração
+
+- Socket: 1700
+
+- Frequência Base: 3.2 GHz
+
+- Frequência Core Turbo: Até 5.2 Ghz
+
+- Intel Smart Cache: 30MB
+
+- Núcleos: 16 (8P+8E)
+
+- Threads: 24
+
+- TDP: 125W
+
+ 
+
+Desbloqueado:
+
+- Sim
+
+ 
+
+Velocidade de memória:
+
+- DDR5: 4800MHz
+
+- DDR4: 3200MHz
+
+ 
+
+Gráficos:
+
+- Intel® UHD Graphics 770
+
+ 
+
+Compatibilidade de Chipset:
+
+- Intel® 600 Series Chipset
+
+ 
+
+Tecnologia Intel® vPro™:
+
+- Sim
+
+ 
+
+Suporte a PCIe Gen 5.0, 4.0, 3.0:
+
+- Sim
+
+ 
+
+Conteúdo da embalagem:
+
+- 1 x Processador Intel Core i9-12900K
+
+Garantia:
+1 ano de garantia (9 meses de garantia contratual junto ao fabricante + 3 meses referentes à garantia legal, nos termos do artigo 26, II, do Código de Defesa do Consumidor)
+
+Peso:
+645 gramas (bruto com embalagem)`,
+    img: i912900K,
+    imgback: i912900Kback,
+  },
+  {
+    category: "Processors",
+    id: "Intel-Core-i7-10700K",
+    name: "Processador Intel Core i7-10700K, Cache 16MB, 3.8GHz (5.1GHz Max Turbo), LGA 1200 - BX8070110700K",
+    price: 759,
+    description: `Características: 
+
+- Marca: Intel 
+
+- Modelo: BX8070110700K 
+
+
+
+Especificações: 
+
+- Litografia: 14 nm 
+
+- Soquetes suportados: FCLGA1200 
+
+- Configuração máxima da CPU: 1 
+
+- Especificação de solução térmica: PCG 2015C 
+
+  
+
+Atuação: 
+
+- Número de Núcleos: 8 
+
+- Número de Tópicos: 16 
+
+- Frequência: Baseada em processador 3.80 GHz 
+
+- Frequência turbo máxima: 5.10 GHz 
+
+- Cache: 16 MB Intel Smart Cache 
+
+- Velocidade do barramento: 8 GT / s 
+
+- Tecnologia Intel Turbo Boost Max 3.0 Frequência: 5.10 GHz  
+
+- TDP: 95W 
+
+  
+
+Memória: 
+
+- Tamanho máximo de memória (dependendo do tipo de memória): 128GB 
+
+- Tipos de Memória: DDR4-2933 
+
+- Número máximo de canais de memória: 2 
+
+- Largura de banda máxima da memória: 45,8 GB / s 
+
+  
+
+Gráficos do Processador: 
+
+- Gráficos do processador: Intel UHD Graphics 630 
+
+- Frequência: baseada em gráficos 350 MHz 
+
+- Frequência dinâmica máxima de gráficos: 1.20 GHz  
+
+- Memória gráfica de vídeo máximo de 64GB 
+
+- Suporte 4K: 60Hz 
+
+- Resolução máxima (HDMI 1.4): 4096x2160 @ 30Hz 
+
+- Resolução máxima (DP): 4096x2304 a 60Hz 
+
+- Resolução máxima (eDP - Tela plana integrada): 4096x2304 a 60Hz 
+
+- Suporte ao DirectX *: 12 
+
+- Suporte ao OpenGL *: 4.5 
+
+- Vídeo de sincronização rápida Intel 
+
+- Tecnologia Intel InTru 3D 
+
+- Tecnologia Intel Clear Video HD 
+
+- Tecnologia Intel Clear Video 
+
+- Nº de monitores suportados: 3 
+
+  
+
+Opções de expansão: 
+
+- Escalabilidade: 1S Only 
+
+- Revisão do PCI Express: 3.0 
+
+- Configurações PCI Express: Até 1x16, 2x8, 1x8 + 2x4 
+
+- Nº máximo de pistas PCI Express: 16  
+
+  
+
+Tecnologias avançadas: 
+
+- Memória Intel Optane suportada 
+
+- Tecnologia Intel Turbo Boost Max 3.0 
+
+- Tecnologia Intel Hyper-Threading 
+
+- Tecnologia de virtualização Intel (VT-x) 
+
+- Tecnologia de virtualização Intel para E/S direcionada (VT-d) 
+
+- Intel VT-x com tabelas de páginas estendidas (EPT)  
+
+- Intel 64  
+
+- Conjunto de instruções: 64 bits 
+
+- Extensões do conjunto de instruções: Intel SSE4.1, Intel SSE4.2, Intel AVX2 
+
+- Tecnologia Intel SpeedStep aprimorada 
+
+- Tecnologias de mon
+
+ 
+
+Garantia do Fornecedor
+
+3 meses
+
+Peso:
+300 gramas (bruto com embalagem)`,
+    img: i710700K,
+    imgback: i710700Kback,
+  },
+  {
+    category: "Processors",
+    id: "Intel-I9-12900kf",
+    name: "Processador Core I9 Processador Bx8071512900kf Hexa Core I9 12900kf 3,2Ghz 30mb Cache S Cooler Lga1700",
+    price: 699,
+    description: `CARACTERÍSTICAS:
+- Marca: Intel
+- Modelo: BX8071512900KF
+
+ESPECIFICAÇÕES:
+
+- Frequência base: 3,20GHz
+- Intel Smart Cache: 30M
+- Cores e Threads: 16 /24
+- Velocidade de memória: DDR4 e DDR5
+- Velocidade de memória suportada: 4800MHz - 3200MH
+- Frequência Intel Single Core Turbo: 5,1GHz
+- Frequência Intel All Core Turbo: 5,2GHz
+- Compatibilidade de Chipset: 600 Series
+- TDP: 125wats
+- Tecnologias Intel Intel Deep Learning Boost (Intel DL Boost) Intel Virtualization Technology Intel AES New Instructions (Intel AES-NI) Intel AVX2 Intel BIOS Guard Intel Boot Guard Intel OS Guard Intel Identity Protection Technology (Intel IPT)
+
+CONTEÚDO DA EMBALAGEM:
+
+- Processador Core I9 Processador Bx8071512900kf Hexa Core I9 12900kf 3,2Ghz 30mb Cache S Cooler Lga1700
+
+ 
+
+Altura Real
+
+5
+
+ 
+
+Largura Real
+
+11
+
+ 
+
+Profundidade Real
+
+12
+
+ 
+
+Garantia do Fornecedor
+
+3 meses
+
+ 
+
+Fabricante
+
+Intel
+
+ 
+
+Modelo
+
+BX8071512900KF
+
+Peso:
+90 gramas (bruto com embalagem)`,
+    img: I912900kf,
+    imgback: I912900kfback,
+  },
+  {
+    category: "Processors",
+    id: "AMD-Ryzen-9-5900X",
+    name: "Processador AMD Ryzen 9 5900X, 3.7GHz, 70MB, 105W, AM4, S/ Cooler",
+    price: 689,
+    description: `CARACTERÍSTICAS:
+- Marca: AMD
+- Modelo: 9 5900X
+
+ESPECIFICAÇÕES:
+- Cores: 12
+- Threads: 24
+- Frequência de Operação: 3.7 GHz
+- Frequência Max Turbo: 4.8 GHz
+- Cache L2: 6MB
+- Cache L3: 64MB
+- TDP: 105w
+- Chiplets: 2x CCD (7nm) 1x IOD (12nm)
+- PCIe: 24x PCIe Gen 4
+
+CONTEÚDO DA EMBALAGEM:
+- Processador AMD Ryzen 9 5900X 3.7ghz, 70MB, 105W, AM4 S/Cooler.
+
+ 
+
+Garantia do Fornecedor
+
+3 meses
+
+ 
+
+Modelo
+
+100-100000061WOF
+
+ 
+
+Linha
+
+Ryzen 9
+
+Peso:
+500 gramas (bruto com embalagem)`,
+    img: Ryzen95900X,
+    imgback: Ryzen95900Xback,
+  },
 ];
 
 export default processors;
 
 /*
-   {
-    category: "Processors",
-    id: "",
-    name: "",
-    price: 0,
-    description: ``,
-    img: '',
-    imgback: '',    
-  },
-  */
+
+  {
+  category: "Processors",
+  id: "",
+  name: "",
+  price: 0,
+  description: ``,
+  img: '',
+  imgback: '',    
+},
+
+
+*/
