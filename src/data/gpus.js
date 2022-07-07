53708ccc2d27d825de2ebde55548b81970108a15
a4 <gtpepi@hotmail.com>
2022-07-07 16:49:46 -0300

    Update GPU database

--- src/data/gpus.js
+++ src/data/gpus.js
@@ -8,6 +8,12 @@ import RX6800XTRed from "../img/gpu/RX6800XTRed.jpg";
 import RX6800XTRedback from "../img/gpu/RX6800XTRedback.jpg";
 import tufRX6700XT from "../img/gpu/tufRX6700XT.jpg";
 import tufRX6700XTback from "../img/gpu/tufRX6700XTback.jpg";
+import rx550 from "../img/gpu/rx550.jpg";
+import rx550back from "../img/gpu/rx550back.jpg";
+import rx6900xt from "../img/gpu/rx6900xt.jpg";
+import rx6900xtback from "../img/gpu/rx6900xtback.jpg";
+import gtx3050 from "../img/gpu/gtx3050.jpg";
+import gtx3050back from "../img/gpu/gtx3050back.jpg";
 
 const gpus = [
   {
@@ -487,6 +493,328 @@ Peso:
 1850 gramas (bruto com embalagem)`,
     img: tufRX6700XT,
     imgback: tufRX6700XTback,
+  },
+  {
+    category: "GPUs",
+    id: "AMD-Radeon-RX-550 ",
+    name: "Placa de Vídeo Sapphire AMD Radeon RX 550 Pulse 4G, GDDR5 - 11268-01-20G",
+    price: 149,
+    description: `Características:
+
+- Marca: Sapphire
+
+- Modelo: 11268-01-20G
+
+ 
+
+Especificações:
+
+ 
+
+GPU:
+
+- 512 processadores de fluxo
+
+- 14 nm
+
+- 4th Graphics Core Next (GCN)
+
+- Clock de 1206 MHz Boost Engine
+
+ 
+
+Memória:
+
+- Barramento de memória de 128 bits
+
+- Tipo de memória GDDR5
+
+- Clock de memória eficaz de 1750 MHz
+
+- 4 GB Tamanho
+
+ 
+
+Suporte BIOS:
+
+- UEFI BIOS
+
+ 
+
+Monitores:
+
+- Máximo 3 saídas
+
+ 
+
+Saída:
+
+- 1 x DVI-D
+
+- 1 x HDMI
+
+- 1 x DisplayPort 1.4
+
+ 
+
+Resolução:
+
+- 3840x2160 (120 Hz) Pixel DisplayPort
+
+- 2560x1600 (60Hz) Pixel Dual Link DVI
+
+- 3840 × 2160p (60 Hz) Resolução HDMI
+
+ 
+
+API:
+
+- OpenGL 4.5
+
+- OpenCL 2.0
+
+- DirectX 12
+
+- Modelo Shader 5.0
+
+ 
+
+Recursos:
+
+- AMD CrossFire
+
+- Tecnologia FreeSync
+
+- AMD Eyefinity
+
+- AMD Xconnect pronto
+
+- DirectX 12 Otimizado
+
+- Preparado para HDR
+
+- Controle de segmentação de taxa de quadros
+
+- Controle Inteligente do Ventilador III
+
+- Black Diamond Choke 4
+
+- Protecção contra fusíveis
+
+- Radeon Chill
+
+- Radeon Revive
+
+ 
+
+Refrigeração:
+
+- Ventilador simples
+
+ 
+
+Conteúdo da Embalagem:
+
+- 01 Placa de Vídeo VGA AMD Sapphire Radeon RX 550
+
+Garantia:
+1 ano de garantia (9 meses de garantia contratual junto ao fabricante + 3 meses referentes à garantia legal, nos termos do artigo 26, II, do Código de Defesa do Consumidor)
+
+Peso:
+511 gramas (bruto com embalagem)`,
+    img: rx550,
+    imgback: rx550back,
+  },
+  {
+    category: "GPUs",
+    id: "Gigabyte-Rx-6900XT",
+    name: "Placa de Vídeo Gigabyte Radeon RX 6900 XT, 16 GB, GDDR6, RGB Fusion - GV-R69XTGAMING OC-16GD",
+    price: 1049,
+    description: `Características:
+
+- Marca: Gigabyte
+
+- Modelo: GV-R69XTGAMING OC-16GD
+
+ 
+
+Especificações:
+
+
+Processador:
+
+- Radeon RX 6900 XT
+
+ 
+
+Core Clock:
+
+- Boost Clock: até 2285 MHz (cartão de referência: 2250 MHz)
+
+- Game Clock: até 2050 MHz (cartão de referência: 2015 MHz)
+
+ 
+
+Desempenho:
+
+- Processadores de fluxo: 5120
+
+- Processo tecnológico: 7 nm
+
+- Card Bus: PCI-E 4.0 x 16
+
+- Formulário PCB: ATX
+
+- DirectX: 12 Ultimate
+
+- OpenGL: 4,6
+
+- PSU recomendado: 850W
+
+- Conectores de energia: 3x 8 pinos
+
+ 
+
+Memória: 
+
+- Clock: 16000 MHz
+
+- Capacidade: 16 GB
+
+- Tipo: GDDR6
+
+- Barramento: 256 bits
+
+- Largura de banda: 512 GB/s
+
+ 
+
+Tela:
+
+- Resolução máxima digital: 7680x4320
+
+- Multi-view: 4
+
+ 
+
+Dimensões (L x W x H):
+
+- 286 x 118 x 58 mm
+
+ 
+
+Conexão:
+
+- 2x DisplayPort 1.4a 
+
+- 2x HDMI 2.1
+
+ 
+
+Conteúdo da embalagem:
+
+- 1x Placa de Vídeo Gigabyte Radeon RX 6900 XT
+
+- 1x Guia rápido
+
+Garantia:
+1 ano de garantia (9 meses de garantia contratual junto ao fabricante + 3 meses referentes à garantia legal, nos termos do artigo 26, II, do Código de Defesa do Consumidor)
+
+Peso:
+1635 gramas (bruto com embalagem)`,
+    img: rx6900xt,
+    imgback: rx6900xtback,
+  },
+  {
+    category: "GPUs",
+    id: "Asus-GTX-3050",
+    name: "Placa de Vídeo Asus NVIDIA GeForce RTX 3050 OC, 8GB, GDDR6, Ray Tracing - 90YV0HH0-M0NA00",
+    price: 559,
+    description: `Características:
+
+- Marca: Asus
+
+- Modelo: DUAL-RTX3050-O8G
+
+
+Especificações:
+
+- Motor gráfico: núcleos CUDA: 2560
+
+- OpenGL: OpenGL 4.6
+
+- Interface de memória: 128 bits
+
+- Memória de vídeo: 8GB GDDR6
+
+- Velocidade da Memória: 14 Gbps
+
+- Conector de alimentação: 1 x 8 pinos
+
+- Compatível com HDCP: HDCP 2.3
+
+- Máx. Resolução: 7680 x 4320
+
+- Suporte máximo de exibição: 4
+
+ 
+
+E/S:
+
+- 1 x HDMI 2.1 nativo
+
+- 3 x Native DisplayPort 1.4a
+
+ 
+
+Operacional:
+
+- Fonte de alimentação recomendada: 550W
+
+- Padrão de barramento: PCIE 4.0
+
+ 
+
+Pacote de software:
+
+- ASUS GPU Tweak II e GeForce Game Ready Driver & Studio Driver: faça o download de todos os softwares do site de suporte.
+
+ 
+
+Slots:
+
+- 2 Slots
+
+ 
+
+Dimensão:
+
+Tamanho do cartão (mm/polegada):
+
+- 200 x 123 x 38 milímetros
+
+- 7,87 x 4,84 x 1,496 polegadas
+
+Caixa de cores (mm):
+
+- 303 x 220 x 55 milímetros
+
+
+Conteúdo da Embalagem:
+
+- 1x Placa de Vídeo Asus
+
+- 1x Cartão de Coleta
+
+- 1 x Manual de configuração de velocidade
+
+Garantia:
+1 ano de garantia (9 meses de garantia contratual junto ao fabricante + 3 meses referentes à garantia legal, nos termos do artigo 26, II, do Código de Defesa do Consumidor)
+
+Peso:
+898 gramas (bruto com embalagem)`,
+    img: gtx3050,
+    imgback: gtx3050back,
   },
 ];
 
