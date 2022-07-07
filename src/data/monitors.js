129840a7d0e0402b2ed6a768e62dbccbae408b83
giovanniPepi <gtpepi@hotmail.com>
2022-07-07 20:41:39 -0300

    Update monitors

--- src/data/monitors.js
+++ src/data/monitors.js
@@ -1,7 +1,19 @@
 import asusled32 from "../img/monitor/asusled32.jpg";
 import asusled32back from "../img/monitor/asusled32back.jpg";
-import TF315.jpg from "../img/monitor/TF315.jpg";
-import TF315back.jpg from "../img/monitor/TF315back.jpg";
+import TF315 from "../img/monitor/TF315.jpg";
+import TF315back from "../img/monitor/TF315back.jpg";
+import OdysseyG5 from "../img/monitor/OdysseyG5.jpg";
+import OdysseyG5back from "../img/monitor/OdysseyG5back.jpg";
+import OdysseyG727 from "../img/monitor/OdysseyG727.jpg";
+import OdysseyG727back from "../img/monitor/OdysseyG727back.jpg";
+import Acer27IPS from "../img/monitor/Acer27IPS.jpg";
+import Acer27IPSback from "../img/monitor/Acer27IPSback.jpg";
+import gigabyte27 from "../img/monitor/gigabyte27.jpg";
+import gigabyte27back from "../img/monitor/gigabyte27back.jpg";
+import TUF27 from "../img/monitor/TUF27.jpg";
+import TUF27back from "../img/monitor/TUF27back.jpg";
+import rogSwift32 from "../img/monitor/rogSwift32.jpg";
+import rogSwift32back from "../img/monitor/rogSwift32back.jpg";
 
 const monitors = [
   {
@@ -302,6 +314,949 @@ Peso:
     img: TF315,
     imgback: TF315back,
   },
+  {
+    category: "monitors",
+    id: "Samsung-Odyssey-G5-34'",
+    name: "Monitor Gamer Samsung Odyssey G5 34' VA, Curvo, Wide, 165 Hz, 2K QHD, 1ms, FreeSync Premium, HDR10, HDMI/DisplayPort - LC34G55TWWLXZD",
+    price: 759,
+    description: `Características:
+
+- Marca: Samsung
+
+- Modelo: LC34G55TWWLXZD
+
+ 
+
+Especificações:
+
+ 
+
+Tela:
+
+- Tamanho de Tela: 34"
+
+- Plano / Curvo: Curvo
+
+- Área de Display Ativo (HxV): 797.22 x 333.72 mm
+
+- Curvatura da tela: 1000R
+
+- Proporção de Tela: 21:9
+
+- Tipo de painel: VA
+
+- Brilho (Típico): 250 cd/㎡
+
+- Brilho (mínimo): 200 cd/㎡
+
+- Contraste Estático: 2,500:1 (padrão)
+
+- Tipo de Contraste: Mega ∞ DCR
+
+- HDR(High Dynamic Range): HDR10
+
+- Resolução: 3,440 x 1,440
+
+- Tempo de resposta: 1ms (MPRT)
+
+- Ângulo de visão (horizontal / vertical): 178°(H)/178°(V)
+
+- Suporte de Cor: Máx. 16.7M
+
+- Taxa de atualização: Máx. 165Hz
+
+ 
+
+Recursos:
+
+- Eye Saver Mode
+
+- Flicker Free
+
+- Picture-By-Picture
+
+- Certificação Windows: Windows 10
+
+- FreeSync Premium
+
+- Off Timer Plus
+
+- Screen Size Optimizer
+
+- Black Equalizer
+
+- Low Input Lag Mode
+
+- Refresh Rate Optimizor
+
+- Super Arena Gaming UX
+
+ 
+
+Interface:
+
+- Display sem fio: Não
+
+- D-Sub: Não
+
+- DVI: Não
+
+- Dual Link DVI: Não
+
+- Display Port 1.4: 1 EA
+
+- Display Port Out: Não
+
+- Mini-Display Port: Não
+
+- HDMI 2.0: 1 EA
+
+- Entrada de áudio: Não
+
+- Fones de ouvido
+
+- Portas USB: Não
+
+- USB-C: Não
+
+ 
+
+Áudio:
+
+- Alto-falante: Não
+
+- Soundbar USB (pronto): Não
+
+ 
+
+Condições Operacionais:
+
+- Temperatura: 40 ℃
+
+- Umidade: 10 ~ 80, sem condensação
+
+ 
+
+Calibração:
+
+- Modo de cores: Personalizado / FPS / RTS / RPG / AOS / Cinema / Contraste dinâmico
+
+ 
+
+Design:
+
+- Cor: Preto
+
+- Tipo de Suporte: SIMPLES
+
+- Inclinação Ajustável: -2 ~ 18
+
+- Montagem de parede: 75 x 75 mm
+
+ 
+
+Alimentação:
+
+- Voltagem: AC 100~240V
+
+- Consumo de Energia: 50W
+
+- Consumo de Energia (DPMS): 0.5W
+
+- Consumo de energia (modo desligado): 0.5W
+
+- Tipo: Adaptador externo
+
+ 
+
+Dimensões:
+
+- Com suporte: 806.6 x 475.3 x 272.6 mm
+
+- Sem suporte: 806.6 x 380.9 x 152.7 mm
+
+ 
+
+Conteúdo da Embalagem:
+
+- Monitor Gamer Samsung
+
+- Cabo DisplayPort
+
+- Cabo de energia (1.5 m)
+
+- Manual do Usuário
+
+Garantia:
+1 ano de garantia (9 meses de garantia contratual junto ao fabricante + 3 meses referentes à garantia legal, nos termos do artigo 26, II, do Código de Defesa do Consumidor)
+
+Peso:
+17326 gramas (bruto com embalagem)`,
+    img: OdysseyG5,
+    imgback: OdysseyG5back,
+  },
+  {
+    category: "monitors",
+    id: " Samsung-Odyssey-G7-27'",
+    name: "Monitor Gamer Samsung Odyssey G7 27' LED, Curvo, Wide, 240 Hz, 2K QHD, 1ms, FreeSync Premium, HDR 600, HDMI/DisplayPort - LC27G75TQSLXZD",
+    price: 749,
+    description: `Características:
+
+- Marca: Samsung
+
+- Modelo: LC27G75TQSLXZD
+
+
+Especificações:
+
+
+Tela:
+
+- Tamanho da tela (cm): 68.4
+
+- Tamanho de Tela (Class): 27
+
+- Plano / Curvo: Curvo
+
+- Área de Display Ativo (HxV) (mm): 596.736 x 335.664mm
+
+- Curvatura da tela: 1000R
+
+- Proporção de Tela: 16:9
+
+- Tipo de painel: VA
+
+- Brilho (Típico): 350cd/m2
+
+- Peak Brightness (Típico): 600cd/m2 cd/㎡
+
+- Brilho (mínimo): 300cd/m2
+
+- Contraste Estático: 2,500:1(Típico)
+
+- Tipo de Contraste: Mega ∞ DCR
+
+- HDR(High Dynamic Range): VESA DisplayHDR 600
+
+- Resolução: 2,560 x 1,440
+
+- Tempo de resposta: 1ms(GtG)
+
+- Ângulo de visão (horizontal / vertical): 178°(H)/178°(V)
+
+- Suporte de Cor: Máx 1.07B
+
+- Color Gamut (DCI Coverage): Típico 95%, Min 88%
+
+- sRGB Coverage: Típico 125
+
+- Taxa de atualização: Máx 240Hz
+
+
+Recursos:
+
+- Eye Saver Mode: Sim
+
+- Flicker Free: Sim
+
+- Picture-In-Picture: Sim
+
+- Picture-By-Picture: Sim
+
+- Quantum Dot Color: Sim
+
+- USB Super Charging: Sim
+
+- Certificação Windows: Windows 10
+
+- G-Sync: Compatível com G-Sync
+
+- Smart Eco Saving: Sim
+
+- Off Timer Plus: Sim
+
+- Screen Size Optimizer: Sim
+
+- Black Equalizer: Sim
+
+- Low Input Lag Mode: Sim
+
+- Refresh Rate Optimizor: Sim
+
+- Super Arena Gaming UX: Sim
+
+
+Interface:
+
+- Display sem fio: Não
+
+- D-Sub: Não
+
+- DVI: Não
+
+- Dual Link DVI: Não
+
+- Display Port: 2 EA
+
+- Versão do Display Port: 1.4
+
+- Display Port Out: Não
+
+- Mini-Display Port: Não
+
+- HDMI: 1 EA
+
+- Versão HDMI: 2
+
+- Entrada de áudio: Não
+
+- Fones de ouvido: Sim
+
+- Portas USB: 2
+
+- Versão hub USB: 3
+
+- USB-C: Não
+
+- USB-C Charging Power: Não
+
+
+Áudio:
+
+- Alto-falante: Não
+
+- Soundbar USB (pronto): Não
+
+
+Condições operacionais:
+
+- Temperatura: 10~40 ℃
+
+- Umidade: 10~80, sem condensação
+
+
+Calibração:
+
+- Ajuste de fábrica: Sim
+
+- Modo de cores: Brilho alto/Personalizado/FPS/RTS/RPG/sRGB/AOS/Cinema/Contraste dinâmico
+
+- Factory Calibration Report: Sim
+
+
+Design:
+
+- Tipo de Suporte: Pivot (mais do que 100MM)
+
+- Base de altura ajustável: 120
+
+- Inclinação Ajustável: -9 ~ 13
+
+- Giro (esquerda/direita): -15 ~ +15
+
+- giratório: -2 ~ +92
+
+- Montagem de parede: 100 x 100
+
+
+Alimentação:
+
+- Alimentação de Energia: AC 100~240V
+
+- Consumo de Energia: 100 W
+
+- Consumo de Energia (DPMS): ≤0.5 W
+
+- Consumo de energia (modo desligado): ≤0.5 W
+
+- Tipo: External Adaptor
+
+
+Conteúdo da Embalagem:
+
+- Monitor Gamer Samsung LED 27'
+
+- Comprimento do cabo de força: 1.5 m
+
+- Cabo DP: Sim
+
+- Cabo USB 3.0: Sim
+
+Garantia:
+1 ano de garantia (9 meses de garantia contratual junto ao fabricante + 3 meses referentes à garantia legal, nos termos do artigo 26, II, do Código de Defesa do Consumidor)
+
+Peso:
+19230 gramas (bruto com embalagem)`,
+    img: OdysseyG727,
+    imgback: OdysseyG727back,
+  },
+  {
+    category: "monitors",
+    id: "Acer-27-IPS",
+    name: "Monitor Gamer Acer 27 IPS, 165 Hz, 2K QHD, 0.4ms, G-Sync, 100% sRGB, HDMI/DisplayPort, Ajuste de Altura, Som Integrado - Predator XB271HU",
+    price: 699,
+    description: `Características:
+
+- Marca: Acer
+
+- Modelo: UM.HX1AA.004
+
+ 
+
+Especificações:
+
+ 
+
+Físico:
+
+- Cor: Preto
+
+- Dimensões: 614 x 401-551 x 268 mm
+
+- Furação VESA para suporte: 100 x 100mm
+
+ 
+
+Display:
+
+- Tamanho da tela: 27” (16:9)
+
+- Área ativa: 597 x 336mm
+
+- Tipo de painel: Tecnologia IPS (In-Plane Switching)
+
+- Tempo de resposta: 4 ms
+
+- Contraste: 1,000:1 (Nativa)
+
+- Brilho: 350 cd/m²
+
+- Angulo de visão: 178° (Horizontal),178° (Vertical)
+
+- Cores: 16.7 Milhões
+
+- Bits: 8-Bit
+
+ 
+
+Resolução máxima:
+
+- Modo normal: DP: 2560 x 1440 @144Hz
+
+- Modo Overclocking: DP: 2560 x 1440 @165Hz
+
+- Gama de cores: 100% sRGB
+
+ 
+
+Taxa de atualização:
+
+- Modo normal: 144Hz
+
+- Modo Overclocking: até 165Hz
+
+ 
+
+ErgoStand:
+
+- Inclinação: -5°~ 35°
+
+- Suporte Giratório: +/- 30°
+
+- Ajuste de altura: 150mm
+
+- Pivô: +/- 90
+
+ 
+
+Sinais de entrada:
+
+- 1x HDMI (v1.4)
+
+- 1x DisplayPort (v1.2)
+
+- 4x USB Hub 3.0
+
+- Saída de áudio
+
+ 
+
+Recursos:
+
+- Tecnologia NVIDIA G-SYNC
+
+- Tecnologia NVIDIA ULMB
+
+- Ajuste de altura
+
+- Organizador de cabos
+
+- GameView OSD button
+
+- Zero-Frame design
+
+- Certificação ambiental: MPR II
+
+ 
+
+Áudio:
+
+- 2x alto falantes de 2W
+
+ 
+
+Alimentação:
+
+- Fonte de energia: Interna (100V-240V)
+
+ 
+
+Consumo de Energia (ENERGY STAR):
+
+- Off: 0.43W
+
+- Sleep: 0.47W
+
+- On: 32.02W
+
+ 
+
+Conteúdo da Embalagem:
+
+- 1x Monitor Gamer Acer Tela de 27" Full HD WQHD
+
+- 1x Cabo DisplayPort
+
+- 1x Cabo HDMI
+
+- 1x Cabo de alimentação
+
+- 1x Cabo USB 3.0
+
+Garantia:
+1 ano de garantia (9 meses de garantia contratual junto ao fabricante + 3 meses referentes à garantia legal, nos termos do artigo 26, II, do Código de Defesa do Consumidor)
+
+Peso:
+14376 gramas (bruto com embalagem)`,
+    img: Acer27IPS,
+    imgback: Acer27IPSback,
+  },
+  {
+    category: "monitors",
+    id: "Gigabyte-27",
+    name: "Monitor Gamer Gigabyte 27´ Curvo, QHD, HDMI/DisplayPort, FreeSync, 165Hz, 1ms, Altura Ajustável - G27QC-A",
+    price: 649,
+    description: `Características:
+
+- Marca: Gigabyte
+
+- Modelo: G27QC-SA
+
+ 
+
+Especificações:
+- Tempo de resposta: 1 ms (MPRT)
+- Potência de entrada CA: 100 a 240 VAC, 50/60 Hz
+- Tipo de adaptador de energia: interno
+- Ajuste de altura: 5,12 "/ 130 mm
+- Ajuste de inclinação: -5 a 20°
+
+ 
+
+Tela:
+
+- Tamanho 27"
+
+- Área da Tela: 23,49 x 13,21 "/ 596,74 x 335,66 mm
+
+- Tipo de painel: LCD
+
+- Curvatura: 1500R
+
+- Resolução: 2560 x 1440
+
+- Proporção da tela: 16: 9
+
+- Pixels por polegada (ppi): 108 ppi
+
+- Brilho Máximo 250 cd / m 2
+
+- Relação de contraste 3000: 1
+
+- Relação de contraste dinâmico 12.000.000: 1
+
+- Taxa de atualização 165 Hz
+
+- Tecnologia Adaptive-Sync G-Sync, FreeSync, Adaptive-Sync
+
+- Profundidade de bits / suporte de cor 8 bits (16,7 milhões de cores)
+
+ 
+
+Gama de cores:
+
+- 92% DCI-P3
+
+- 120% sRGB
+
+ 
+
+Ângulo de visão:
+
+- Horizontal: 178°
+
+- Vertical: 178°
+
+ 
+
+Conectividade:
+
+- 2 x HDMI 2.0
+
+- 1 x DisplayPort 1.4
+
+- 1 x 3,5 mm Saída
+
+- 2 x USB Tipo-A (USB 3.1 / USB 3.2 Gen 1)
+
+ 
+
+Alto-falantes embutidos:
+
+- 2 x 2 W
+
+ 
+
+Consumo de energia:
+
+- 70 W (máximo)
+
+- 0,3 W (espera)
+
+ 
+
+Dimensões (L x A x P):
+
+- 24,0 x 20,9 x 8,0 "/ 610,3 x 531,0 x 203,0 mm (com suporte)
+
+- 24,0 x 14,5 x 3,3" / 610,3 x 367,4 x 84,5 mm (sem suporte)
+
+ 
+
+Conteúdo da embalagem:
+
+- Monitor Gamer Gigabyte 27´ Curvo, QHD, HDMI/DisplayPort, FreeSync, 165Hz, 1ms, Altura Ajustável 
+
+- Cabo de alimentação
+
+- Cabo HDMI
+
+- Cabo Displayport
+
+- Cartão de garantia
+
+Garantia:
+3 anos de garantia (33 meses de garantia contratual junto ao fabricante + 3 meses referentes à garantia legal, nos termos do artigo 26, II, do Código de Defesa do Consumidor)
+
+Peso:
+16561 gramas (bruto com embalagem)`,
+    img: gigabyte27,
+    imgback: gigabyte27back,
+  },
+  {
+    category: "monitors",
+    id: "Asus-TUF-27",
+    name: "Monitor Gamer Asus TUF 27' LED, Curvo, 165 Hz, 2K QHD, 1ms, FreeSync Premium, HDR 10, 120% sRGB, HDMI/DisplayPort, VESA, Som - VG27WQ1B",
+    price: 599,
+    description: `Características:
+
+- Marca: ASUS
+
+- Modelo: 90LM0671-B011X0
+
+ 
+
+Especificações:
+
+ 
+
+Tela:
+
+- Tamanho: 27 (polegadas)
+
+- Curvatura: 1500R
+
+- Proporção: 16:9
+
+- Espaço de cor (DCI-P3): 98%
+
+- Espaço de cor (sRGB): 120%
+
+- Tipo de painel: VA
+
+- Backlight tipo: LED
+
+- Resolução: 2560 x 1440
+
+- Área de visualização da tela (HxV): 596.736 x 335.664 mm
+
+- Superfície da tela: Antirreflexo
+
+- Pixel Pitch:0.233 mm
+
+- Relação de contraste: 3000: 1
+
+- Ângulo de Visão: (CR≧10, H/V): 178°/ 178°
+
+- Tempo de resposta: 1ms MPRT
+
+- Cores da tela: 16,7M
+
+- Flicker-free: Sim
+
+- HDR (High Dynamic Range) Support : HDR10
+
+- Taxa de atualização (Max): 165Hz
+
+ 
+
+Recursos de Vídeo:
+
+- Tecnologia Trace Free: Sim
+
+- GamePlus: Sim
+
+- Luz azul baixa: Sim
+
+- Suporte HDCP: Sim
+
+- Game Visual: Sim
+
+- VRR Technology: FreeSync Premium
+
+- Modo HDR: Sim
+
+ 
+
+Recursos de áudio:
+
+- Alto falante: Sim (2x2W)
+
+ 
+
+Portas I/O:
+
+- 2 x DisplayPort 1.2
+
+- 2 x HDMI(v2.0)
+
+ 
+
+Frequência do sinal digital:
+
+- DisplayPort: 30~185 KHz (H) / 48~165 Hz (V)
+
+- HDMI: 30~185 KHz (H) / 48~144 Hz (V)
+
+- Frequência de sinal analógico :24~82 KHz (H) / 50~75 Hz (V)
+
+ 
+
+Consumo de energia:
+
+- Consumo de energia: <28W
+
+- Modo de economia de energia: <0,5W
+
+- Modo desligado: <0,5W
+
+- Tensão: 100-240V, 50/60Hz
+
+ 
+
+Design mecânico:
+
+- Inclinação: Sim (+20° ~ -5°)
+
+- Girar: Sim (+16° ~ -16°)
+
+- Montagem em parede VESA: 100 x 100 mm
+
+- Trava Kensington: Sim
+
+ 
+
+Dimensões:
+
+- Com suporte (LxAxP): 709.75 x 507.12 x 213.77 mm
+
+- Sem suporte (LxAxP): 709.75 x 426.15 x 87.33 mm
+
+ 
+
+Conteúdo da embalagem:
+
+- Monitor Asus Gamer 27 polegadas WQHD
+
+Garantia:
+3 anos de garantia (33 meses de garantia contratual junto ao fabricante + 3 meses referentes à garantia legal, nos termos do artigo 26, II, do Código de Defesa do Consumidor)
+
+Peso:
+10116 gramas (bruto com embalagem)`,
+    img: TUF27,
+    imgback: TUF27back,
+  },
+  {
+    category: "monitors",
+    id: "Asus-ROG-Swift-32",
+    name: "Monitor Gamer Asus ROG Swift 32' IPS, 175 Hz, 2K QHD, 1ms, Adaptive Sync, HDR 600, 160% sRGB, HDMI/DisplayPort, Ajuste de Altura - PG329Q",
+    price: 1099,
+    description: `Características:
+
+- Marca: ASUS
+
+- Modelo: 90LM06L0-B011X0
+
+ 
+
+Especificações:
+
+ 
+
+Tela:
+
+- Tamanho: 32 (polegadas)
+
+- Proporção: 16:9
+
+- Espaço de cor (DCI-P3): 98%
+
+- Espaço de cor (sRGB): 160%
+
+- Tipo de painel: IPS
+
+- Backlight tipo: LED
+
+- Resolução: 2560x1440
+
+- Área de visualização da tela (HxV): 708,48 x 398,52 mm
+
+- Superfície de exibição: Não ofuscante
+
+- Pixel pitch: 0,155 mm
+
+- Ângulo de Visão: (CR≧10, H/V) : 178°/ 178°
+
+- Brilho (Typ.): 450cd/㎡
+
+- Brilho: (HDR, Peak): 600 cd/㎡
+
+- Ângulo de Visão (CR≧10): 178°/ 178°
+
+- Tempo de resposta : 1ms(GTG)
+
+- Cores do display : 1073.7M (10 bit)
+
+- Flicker-free: Sim
+
+- HDR (High Dynamic Range) Support : HDR10
+
+- Taxa de atualização (Max): 175Hz
+
+ 
+
+Recursos de Vídeo:
+
+- Tecnologia Trace Free: Sim
+
+- Precisão da cor: < 2
+
+- GamePlus: Sim
+
+- Luz azul baixa: Sim
+
+- Suporte HDCP: Sim, 2.2
+
+- Game Visual: Sim
+
+- VRR Technology: G-SYNC™ Compatível
+
+- ELMB Sync: Sim
+
+- HDR Mode: Sim
+
+- Display Widget: Sim
+
+ 
+
+Portas I/O:
+
+- 1 x DisplayPort 1.2
+
+- 2 x HDMI(v2.0)
+
+- 1 x Entrada para fone de ouvido
+
+- 2 x Portas USB
+
+ 
+
+Frequência do sinal digital:
+
+- HDMI :30~225 KHz (H) / 48~165 Hz(V)
+
+- DP: 30~250 KHz (H) / 48 ~175 Hz(V)
+
+ 
+
+Consumo de energia:
+
+- Consumo de energia: <43W
+
+- Modo de economia de energia: <0,5W
+
+- Modo de desligamento: 0,5W
+
+- Voltagem: 100-240 V, 50/60 Hz
+
+ 
+
+Design mecânico:
+
+- Inclinação: Sim (+20° ~ -5°)
+
+- Giratório: Sim (+25° ~ -25°)
+
+- Ajuste de altura: 0 ~ 100 mm
+
+- Montagem em parede VESA: 100 x 100 mm
+
+- Efeito de iluminação (Aura): Aura Sync
+
+- Trava Kensington: Sim
+
+ 
+
+Dimensões:
+
+- Com suporte (LxAxP): 728.18 x 551.83 x 292.56 mm
+
+- Sem suporte (LxAxP): 728.18 x 428.52 x 87.12 mm
+
+ 
+
+Conteúdo da embalagem:
+
+- Monitor Gamer Asus ROG Swift PG329Q
+
+Garantia:
+3 anos de garantia (33 meses de garantia contratual junto ao fabricante + 3 meses referentes à garantia legal, nos termos do artigo 26, II, do Código de Defesa do Consumidor)
+
+Peso:
+24801 gramas (bruto com embalagem)`,
+    img: rogSwift32,
+    imgback: rogSwift32back,
+  },
 ];
 
 export default monitors;
@@ -310,7 +1265,7 @@ export default monitors;
 
 
 {
-  category: "GPUs",
+  category: "monitors",
   id: "",
   name: "",
   price: 0,
